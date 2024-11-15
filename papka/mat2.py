9de200d30935ddbe094619b3df5fe1ed676575f4
IvanMedvedev11 <medvivan11@gmail.com>
2024-11-15 16:38:55 +0700

    Update mat2.py

--- papka/mat2.py
+++ papka/mat2.py
@@ -1,1 +1,7 @@
-
+import random
+x, y = list(map(int, input("Введите размерности матрицы: ").split()))
+matrix = [[random.randint(0, 1000) for i in range(x)] for j in range(y)]
+for i in range(len(matrix)):
+    matrix[i][i], matrix[-i - 1][i] = matrix[-i - 1][i], matrix[i][i]
+for i in range(len(matrix)):
+    print(*matrix[i])
